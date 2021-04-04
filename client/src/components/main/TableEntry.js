b7a7c07e846623be1d041958ba466dcae51dd43f
wolfie-VIP <70851690+wolfie-VIP@users.noreply.github.com>
2021-04-03 21:52:32 -0400

    Update TableEntry.js

--- client/src/components/main/TableEntry.js
+++ client/src/components/main/TableEntry.js
@@ -88,7 +88,7 @@ const TableEntry = (props) => {
                     <WButton className="table-entry-buttons" onClick={() => props.reorderItem(data._id, 1)} wType="texted">
                         <i className="material-icons">expand_more</i>
                     </WButton>
-                    <WButton className="table-entry-buttons" onClick={() => props.deleteItem(data)} wType="texted">
+                    <WButton className="table-entry-buttons" onClick={() => props.deleteItem(data, props.index)} wType="texted">
                         <i className="material-icons">close</i>
                     </WButton>
                 </div>
